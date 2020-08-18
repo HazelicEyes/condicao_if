ec58fcb79ee9521b00c48ab72cba9bab3ca95bc4
HazelicEyes <henriquepromail9863@gmail.com>
2020-08-18 03:50:35 -0300

    Add project files.

---
+++ condicao_if/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace condicao_if
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int idade;
+            idade = Convert.ToInt32(txtIdade.Text);
+            if (idade < 18)
+                MessageBox.Show("Voce ainda é menor de idade", "Mensagem");
+            else
+                MessageBox.Show("Parabens, voce esta apto", "Mensagem");
+        }
+    }
+}
